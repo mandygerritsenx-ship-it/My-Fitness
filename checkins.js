9d3f67c6e2d27a2bfa8705b9eef1f1b34d030c21
mandygerritsenx-ship-it <mandygerritsenx@gmail.com>
2026-05-16 23:07:59 +0300

    Delete checkins.js

--- checkins.js
+++
@@ -1,3 +0,0 @@
-const CHECKINS_KEY='checkins';
-function loadCheckins(){return JSON.parse(localStorage.getItem(CHECKINS_KEY))||[]}
-function saveCheckin(c){const a=loadCheckins();a.push(c);localStorage.setItem(CHECKINS_KEY,JSON.stringify(a))}
