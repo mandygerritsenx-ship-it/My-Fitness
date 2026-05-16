c6997afad650fb5d6ae29eaabdcda25ab2a78533
mandygerritsenx-ship-it <mandygerritsenx@gmail.com>
2026-05-16 23:08:29 +0300

    Delete weight.js

--- weight.js
+++
@@ -1,4 +0,0 @@
-const WEIGHT_KEY='dailyWeights';
-function loadWeights(){return JSON.parse(localStorage.getItem(WEIGHT_KEY))||{}}
-function saveWeight(d,v){const w=loadWeights();w[d]=v;localStorage.setItem(WEIGHT_KEY,JSON.stringify(w))}
-function getSortedWeights(){const w=loadWeights();return Object.keys(w).sort().map(d=>({date:d,value:w[d]}))}
